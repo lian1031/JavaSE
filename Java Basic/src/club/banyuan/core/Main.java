813717f88c6a6c694c02c4257b877127580b027f
lianchen <397984710@qq.com>
2020-12-12 15:48:38 +0800

    远程关联git

--- Java Basic/src/club/banyuan/core/Main.java
+++ Java Basic/src/club/banyuan/core/Main.java
@@ -2,7 +2,7 @@ package club.banyuan.core;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("I'm your father。。");
+        System.out.println("I'm your father");
     }
 
 }
